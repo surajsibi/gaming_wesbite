2cf8db2d0e8a745790f75780b7261fcdbc0ae723
surajsibi <surajsibi2022@gmail.com>
2025-06-09 11:24:52 +0530

    footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
 
         <div className='flex justify-center gap-4 md:justify-start'>
           {links.map((link)=>(
-            <a key={icon} href='#' target='_blank' rel='noopener noreferrer' className='text-black transition-colors duration-500 ease-in-out hover:text-white'>
+            <a key={link} href='#' target='_blank' rel='noopener noreferrer' className='text-black transition-colors duration-500 ease-in-out hover:text-white'>
               {link.icon}
             </a>
           ))}
